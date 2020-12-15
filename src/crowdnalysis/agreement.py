36ee7bc5faabd34fe5d365ea92487725c34465d3
Oguz Mulayim <oguz@iiia.csic.es>
2020-12-15 17:00:22 +0100

    #3, add generalized Fleiss' kappa

--- src/crowdnalysis/agreement.py
+++ src/crowdnalysis/agreement.py
@@ -24,6 +24,36 @@ def fleiss_kappa(d: Data, question: str) -> float:
     return kappa
 
 
+def gen_fleiss_kappa(d: Data, question: str, w: np.ndarray=None) -> float:
+    """Return Fleiss' kappa value for the annotation data"""
+    n, *_ = get_n_and_ranges(d, question)
+    kappa = _gen_fleiss_kappa(n, w)
+    return kappa
+
+
+def _gen_fleiss_kappa(r, w=None):
+    # r[i][l] -> number of raters that assigned item i to category l
+    n, q = r.shape
+    if w is None:
+        w = np.identity(q)
+
+    r_star = np.dot(r, np.transpose(w))
+    r_i = np.sum(r, axis=1)
+    # print(r_i)
+    assert(np.all(r_i>1))
+    den_i = r_i * (r_i-1)
+    # print(den_i)
+    num_i_k= r * (r_star - 1)
+    num_sum_i = np.sum(num_i_k, axis=1)
+    # print(num_sum_i)
+    p_0 = np.sum(num_sum_i/den_i)/n
+    # print("p_0:",p_0)
+    pi_k = np.sum(r/r_i[:,np.newaxis], axis=0)/n
+    # print(pi_k)
+    p_c = np.dot(np.dot(pi_k, w), pi_k)
+    return (p_0-p_c)/(1-p_c)
+
+
 def full_agreement_percentage(d: Data, question: str) -> float:
     """Return the percentage of all annotations for the question"""
     n, I, *_ = get_n_and_ranges(d, question)
