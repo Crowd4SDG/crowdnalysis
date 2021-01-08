1dd2a2651f1dc250c246f7e21a0cceefb5629964
Oguz Mulayim <oguz@iiia.csic.es>
2021-01-08 16:18:00 +0100

    #5, 1) add image url to Data.df; 2) add Data.data_src attr

--- src/crowdnalysis/data.py
+++ src/crowdnalysis/data.py
@@ -16,6 +16,7 @@ class Data:
         """
         # Make instance attributes' definitions explicit
         self.df = None
+        self.data_src = None
         self.questions = None
         self.task_ids = None
         self.task_index_from_id = None
@@ -25,7 +26,7 @@ class Data:
         self.n_annotators = None
 
     @classmethod
-    def from_df(cls, df, task_id_col_name="task_id", annotator_id_col_name="annotator_id",
+    def from_df(cls, df, data_src=None, task_id_col_name="task_id", annotator_id_col_name="annotator_id",
                 questions=None, task_ids=None, categories=None):
         """ 
         Create a Data object from dataframe df.
@@ -34,6 +35,8 @@ class Data:
         questions, each answer in one column
         """
         d = Data()
+
+        d.data_src = data_src
 
         if questions is None:
             questions = list(df.columns)
@@ -63,27 +66,62 @@ class Data:
         return d
 
     @classmethod
-    def from_pybossa(cls, file_name, questions, preprocess=lambda x:x, task_ids=None, categories=None, other_columns=[]):
-        """ 
-        Create a Data object from a pybossa file.
-        """
-        df = pd.read_csv(file_name)
+    def _preprocess(cls, df, questions, preprocess=lambda x: x, other_columns=[]):
+        df = df.copy()
         columns_to_retain = ["task_id", "user_id"] + questions + other_columns
         df = preprocess(df)
         df = df[columns_to_retain]
-        #print(df)
-
-        return cls.from_df(df, annotator_id_col_name="user_id", task_ids=task_ids, categories=categories)
+        return df
 
     @classmethod
-    def from_mturk(cls, file_name, questions, preprocess=lambda x: x, task_ids=None, categories=None):
-        """Create a Data object from an amazon mturk file."""
-        return cls.from_pybossa(file_name, questions, preprocess, task_ids, categories)
+    def _from_single_file(cls, file_name, questions, data_src=None, preprocess=lambda x: x, task_ids=None, categories=None,
+                          other_columns=[]):
+        """
+        Create a Data object from a file.
+        """
+        df = pd.read_csv(file_name)
+        # print("_from_single_file -> df columns before preprocessing: ", df.columns)
+        df = cls._preprocess(df, questions, preprocess, other_columns)
+        # print("_from_single_file -> df columns AFTER preprocessing: ", df.columns)
+
+        return cls.from_df(df, data_src=data_src, annotator_id_col_name="user_id", questions=questions, task_ids=task_ids, categories=categories)
 
     @classmethod
-    def from_aidr(cls, file_name, questions, preprocess=lambda x: x, task_ids=None):
+    def from_pybossa(cls, file_name, questions, data_src=None, preprocess=lambda x: x, task_ids=None, categories=None,
+                     task_info_file=None, task_file=None, field_task_key="info_media_0", other_columns=[]):
+        """ 
+        Create a Data object from a Pyossa file.
+        """
+
+        def get_tasks(t_csv, ti_csv, field_task_id, field_task_key):
+            t_df = pd.read_csv(t_csv)
+            t_df = t_df[field_task_key]
+            ti_df = pd.read_csv(ti_csv)
+            ti_df = ti_df[field_task_id]
+            t_df = pd.merge(t_df, ti_df, left_index=True, right_index=True)
+            # print("get_tasks {} {} ({}) -> \n{}".format("PyBossa", task_file, len(t_df.index), t_df))
+            return t_df
+
+        df = pd.read_csv(file_name)
+        if task_info_file is not None:
+            t_df = get_tasks(task_file, task_info_file, "task_id", field_task_key)
+            df = pd.merge(df, t_df, how="left", left_on="task_id", right_on="task_id")
+        # print("from_pybossa -> df columns before preprocessing: ", df.columns)
+        df = cls._preprocess(df, questions, preprocess, other_columns=other_columns)
+        #  print("from_pybossa -> df columns AFTER preprocessing: ", df.columns)
+        # print(df)
+        return cls.from_df(df, data_src=data_src, annotator_id_col_name="user_id", questions=questions, task_ids=task_ids, categories=categories)
+
+    @classmethod
+    def from_mturk(cls, file_name, questions, data_src=None, preprocess=lambda x: x, task_ids=None, categories=None, other_columns=[]):
+        """Create a Data object from an Amazon MTurk file."""
+        return cls._from_single_file(file_name, questions, data_src, preprocess, task_ids, categories, other_columns)
+
+    @classmethod
+    def from_aidr(cls, file_name, questions, data_src=None, preprocess=lambda x: x, task_ids=None, other_columns=[]):
         """Create a Data object from an AIDR file."""
-        return cls.from_pybossa(file_name, questions, preprocess, task_ids)  # Note: Does NOT send 'categories' arg
+        # Note: Does NOT send 'categories' arg
+        return cls._from_single_file(file_name, questions, data_src, preprocess, task_ids, other_columns=other_columns)
 
     def set_questions(self, questions):
         self.questions = questions
@@ -127,3 +165,22 @@ class Data:
             #print(self.df[q].dtype)
             categories[q] = self.df[q].dtype
         return categories
+
+    def get_field(self, task_indices, field, unique=False):
+        """Return the `field values` within the `self.df` DataFrame that corresponds to the given `task_indices`
+
+        Args:
+            task_indices (np.ndarray): array of integer values on the `task_index` column.
+            field (str): `data.df` DataFrame column name
+            unique (bool): If True, only unique values will be returned
+
+        Returns:
+            np.ndarray: `field` values corresponding to and in the order of the given `task_indices`.
+        """
+        df_vals = self.df[self.df.task_index.isin(task_indices)][["task_index", field]]  # .isin() loses the order
+        df_ind = pd.DataFrame(task_indices, columns=["task_index"])
+        df_vals = df_ind.merge(df_vals, how="left", left_on="task_index", right_on="task_index")  # Preserve the order
+        df_vals = df_vals[field]
+        if unique:
+            df_vals = df_vals.drop_duplicates()
+        return df_vals.to_numpy()
