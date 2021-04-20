29c596ef9f9494513fa0f2f306b8b9c54bd03d50
Jesus Cerquides <cerquide@iiia.csic.es>
2021-04-20 17:43:52 +0200

    Improved DS test

--- src/crowdnalysis/test/test_dawid_skene.py
+++ src/crowdnalysis/test/test_dawid_skene.py
@@ -4,7 +4,7 @@ from . import close, distance
 import numpy as np
 
 def sample():
-    dgp = DawidSkene.DataGenerationParameters(n_tasks=1000, num_annotations_per_task=10)
+    dgp = DawidSkene.DataGenerationParameters(n_tasks=1000, num_annotations_per_task=20)
     parameters = DawidSkene.Parameters(tau=np.array([0.3, 0.7]), pi=np.array([[[0.9, 0.1], [0.2, 0.8]]]))
     ds = DawidSkene()
     problem = ds.sample(dgp, parameters)
