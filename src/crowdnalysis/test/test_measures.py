f73d6ef5da071b13fcdf822b7fa661c6223d9399
Oguz Mulayim <oguz@iiia.csic.es>
2021-04-21 17:04:07 +0200

    Add accuracy tests for multi-label consensus (#8)

--- src/crowdnalysis/test/test_measures.py
+++ src/crowdnalysis/test/test_measures.py
@@ -24,25 +24,25 @@ def test_accuracies_multilabel():
                               [0.23, 0.14, 0.28, 0.35]])  # 3
     real2 = np.array([0, 2, 1, 2, 3])  # different than the consensus
     # Switch 1<->3
-    consensus_none = np.array([[i[0], i[3], i[2], i[1]] for i in consensus_all.tolist()])
-    assert Accuracy.evaluate(real, consensus_all) == 1.0   # [0, 2, 1, 1, 3] vs [0, 2, 1, 1, 3]
-    assert Accuracy.evaluate(real, consensus_none) == 0.4  # [0, 2, 1, 1, 3] vs [0, 2, 3, 3, 1]
+    consensus_switched = np.array([[i[0], i[3], i[2], i[1]] for i in consensus_all.tolist()])
+    assert Accuracy.evaluate(real, consensus_all) == 1.0       # [0, 2, 1, 1, 3] vs [0, 2, 1, 1, 3]
+    assert Accuracy.evaluate(real, consensus_switched) == 0.4  # [0, 2, 1, 1, 3] vs [0, 2, 3, 3, 1]
     assert LSAccuracy.evaluate(real, consensus_all) == 1.0
-    assert LSAccuracy.evaluate(real, consensus_none) == 1.0
+    assert LSAccuracy.evaluate(real, consensus_switched) == 1.0
 
-    assert Accuracy.evaluate(real2, consensus_all) == 0.8   # [0, 2, 1, 2, 3] vs [0, 2, 1, 1, 3]
-    assert Accuracy.evaluate(real2, consensus_none) == 0.4  # [0, 2, 1, 2, 3] vs [0, 2, 3, 3, 1]
+    assert Accuracy.evaluate(real2, consensus_all) == 0.8       # [0, 2, 1, 2, 3] vs [0, 2, 1, 1, 3]
+    assert Accuracy.evaluate(real2, consensus_switched) == 0.4  # [0, 2, 1, 2, 3] vs [0, 2, 3, 3, 1]
     assert LSAccuracy.evaluate(real2, consensus_all) == 0.8
-    assert LSAccuracy.evaluate(real2, consensus_none) == 0.8
+    assert LSAccuracy.evaluate(real2, consensus_switched) == 0.8
 
     # Switch 0<->3, 1<->2
-    consensus_none = np.array([[i[3], i[2], i[1], i[0]] for i in consensus_all.tolist()])
-    assert Accuracy.evaluate(real, consensus_all) == 1.0   # [0, 2, 1, 1, 3] vs [0, 2, 1, 1, 3]
-    assert Accuracy.evaluate(real, consensus_none) == 0.0  # [0, 2, 1, 1, 3] vs [3, 1, 2, 2, 0]
+    consensus_switched = np.array([[i[3], i[2], i[1], i[0]] for i in consensus_all.tolist()])
+    assert Accuracy.evaluate(real, consensus_all) == 1.0       # [0, 2, 1, 1, 3] vs [0, 2, 1, 1, 3]
+    assert Accuracy.evaluate(real, consensus_switched) == 0.0  # [0, 2, 1, 1, 3] vs [3, 1, 2, 2, 0]
     assert LSAccuracy.evaluate(real, consensus_all) == 1.0
-    assert LSAccuracy.evaluate(real, consensus_none) == 1.0
+    assert LSAccuracy.evaluate(real, consensus_switched) == 1.0
 
-    assert Accuracy.evaluate(real2, consensus_all) == 0.8   # [0, 2, 1, 2, 3] vs [0, 2, 1, 1, 3]
-    assert Accuracy.evaluate(real2, consensus_none) == 0.2  # [0, 2, 1, 2, 3] vs [3, 1, 2, 2, 0]
+    assert Accuracy.evaluate(real2, consensus_all) == 0.8       # [0, 2, 1, 2, 3] vs [0, 2, 1, 1, 3]
+    assert Accuracy.evaluate(real2, consensus_switched) == 0.2  # [0, 2, 1, 2, 3] vs [3, 1, 2, 2, 0]
     assert LSAccuracy.evaluate(real2, consensus_all) == 0.8
-    assert LSAccuracy.evaluate(real2, consensus_none) == 0.8
+    assert LSAccuracy.evaluate(real2, consensus_switched) == 0.8
