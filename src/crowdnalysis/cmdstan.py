Fixed the non finite gradient error and introduced test to verify it

src/crowdnalysis/cmdstan.py
@@ -173,34 +173,39 @@ class StanMultinomialOptimizeConsensus(AbstractStanOptimizeConsensus):
     def __init__(self):
         AbstractStanOptimizeConsensus.__init__(self, "Multinomial")
 
-    def tau_prior(self, ann, k, alpha=1.):
+    def tau_estimate(self, ann, k):
         unique, counts = np.unique(ann, return_counts=True)
         tp = np.ones(k)
         for i, x in enumerate(unique):
             tp[x - 1] += counts[i]
         tp /= np.sum(tp)
-        return alpha * tp
+        return tp
 
-    def pi_prior(self, k, alpha=1, beta=10):
+    def tau_prior(self, ann, k, alpha=5., beta=5.):
+        return alpha * self.tau_estimate(ann, k) + beta
+
+    def pi_prior(self, k, alpha=1., beta=10):
         return np.ones((k, k)) * alpha + np.identity(k) * beta
 
     def map_data_to_prior(self, k, ann, **kwargs):
-        tau_prior_ = self.tau_prior(ann, k, alpha=5.)
-        pi_prior_ = self.pi_prior(k)
+        tau_prior_ = self.tau_prior(ann, k, alpha=5.)+5.
+        print("tp:", tau_prior_)
+        pi_prior_ = self.pi_prior(k, alpha=5, beta=5)
 
         return {"tau_prior": tau_prior_,
                 "pi_prior": pi_prior_}
 
     def map_data_to_inits(self, ann, k, **kwargs):
         pi_prior_ = self.pi_prior(k)
-        return {'tau': self.tau_prior(ann, k, alpha=1.),
+        return {'tau': self.tau_estimate(ann, k),
                 'pi': (pi_prior_ / np.sum(pi_prior_[0]))}
 
     def map_data_to_args(self, **kwargs):
         # args = {"iter": 2000}
         return {'algorithm': 'LBFGS',
-                'init_alpha': 0.01}
-                # 'output_dir': "."}
+                #'init_alpha': 0.001
+                 'output_dir': "."
+                }
 
     def sample_tasks(self, dgp: DataGenerationParameters, parameters: Optional[Parameters] = None) \
             -> Tuple[int, Optional[np.ndarray]]:

src/crowdnalysis/test/test_cmdstan.py
@@ -4,7 +4,7 @@ from . import close, distance
 import numpy as np
 
 
-def sample():
+def easy_sample():
     dgp = StanMultinomialOptimizeConsensus.DataGenerationParameters(n_tasks=1000, num_annotations_per_task=20)
     parameters = StanMultinomialOptimizeConsensus.Parameters(tau=np.array([0.3, 0.7]),
                                                              pi=np.array([[0.9, 0.1], [0.2, 0.8]]))
@@ -12,17 +12,29 @@ def sample():
     problem = smoc.sample(dgp, parameters)
     return problem, parameters
 
+def sample_non_finite_gradient():
+    dgp = StanMultinomialOptimizeConsensus.DataGenerationParameters(n_tasks=100000, num_annotations_per_task=3)
+    parameters = StanMultinomialOptimizeConsensus.Parameters(tau=np.array([0.05, 0.95]),
+                                                             pi=np.array([[0.999, 0.001], [0.2, 0.8]]))
+    smoc = StanMultinomialOptimizeConsensus()
+    problem = smoc.sample(dgp, parameters)
+    return problem, parameters
+
 
 def test_sampling():
-    problem, parameters = sample()
+    problem, parameters = easy_sample()
     # log.info(problem)
     # log.info(parameters)
 
+def test_all_samples():
+    sample_functions = [easy_sample, sample_non_finite_gradient]
+    for s in sample_functions:
+        _test_fit_and_compute_consensus(s)
 
-def test_fit_and_compute_consensus():
-    problem, parameters = sample()
-    ds = StanMultinomialOptimizeConsensus()
-    consensus, parameters_learned = ds.fit_and_compute_consensus(problem)
+def _test_fit_and_compute_consensus(sample_f):
+    problem, parameters = sample_f()
+    multinomial = StanMultinomialOptimizeConsensus()
+    consensus, parameters_learned = multinomial.fit_and_compute_consensus(problem)
     tau_distance = distance(parameters_learned.tau, parameters.tau)
     log.debug("Distance between learned tau and real tau: %f", tau_distance)
     pi_distance = distance(parameters_learned.pi, parameters.pi)