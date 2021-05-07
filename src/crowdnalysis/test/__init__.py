0bd7c5088b582221dcbc56e5dedc26c55af09449
Oguz Mulayim <oguz@iiia.csic.es>
2021-05-07 18:42:34 +0200

    add 'fit' and 'compute_consensus' tests (#8) . use pytest fixtures . define generic test functions that take stan models as args . read model parameter names from the dictionary to accomodate different models

--- src/crowdnalysis/test/__init__.py
+++ src/crowdnalysis/test/__init__.py
@@ -1,9 +1,11 @@
 import numpy as np
 
+TOLERANCE = 0.05
+
 
 def distance(a: np.ndarray, b: np.ndarray):
     return np.linalg.norm(a-b)
 
 
-def close(a: np.ndarray, b: np.ndarray, eps=0.05):
+def close(a: np.ndarray, b: np.ndarray, eps=TOLERANCE):
     return distance(a, b) < eps
--- src/crowdnalysis/test/test_cmdstan.py
+++ src/crowdnalysis/test/test_cmdstan.py
@@ -1,53 +1,96 @@
-from .. import log
-from ..cmdstan import StanMultinomialOptimizeConsensus
-from . import close, distance
+import dataclasses
+from typing import List, Callable, Type
+
+import pytest
+from dataclasses import dataclass
+
 import numpy as np
 
+from .. import log
+from ..problems import ConsensusProblem
+from ..cmdstan import AbstractStanOptimizeConsensus, StanMultinomialOptimizeConsensus
+from . import close, distance, TOLERANCE
 
-def easy_sample():
+
+@dataclass()
+class SampleForTest:
+    problem: ConsensusProblem
+    parameters: AbstractStanOptimizeConsensus.Parameters
+
+
+def easy_sample() -> SampleForTest:
     dgp = StanMultinomialOptimizeConsensus.DataGenerationParameters(n_tasks=1000, num_annotations_per_task=20)
     parameters = StanMultinomialOptimizeConsensus.Parameters(tau=np.array([0.3, 0.7]),
                                                              pi=np.array([[0.9, 0.1], [0.2, 0.8]]))
     smoc = StanMultinomialOptimizeConsensus()
     problem = smoc.sample(dgp, parameters)
-    return problem, parameters
+    return SampleForTest(problem, parameters)
 
-def sample_non_finite_gradient():
+
+def sample_non_finite_gradient() -> SampleForTest:
     dgp = StanMultinomialOptimizeConsensus.DataGenerationParameters(n_tasks=100000, num_annotations_per_task=3)
     parameters = StanMultinomialOptimizeConsensus.Parameters(tau=np.array([0.05, 0.95]),
                                                              pi=np.array([[0.999, 0.001], [0.2, 0.8]]))
     smoc = StanMultinomialOptimizeConsensus()
     problem = smoc.sample(dgp, parameters)
-    return problem, parameters
+    log.info("sample_non_finite_gradient")
+    log.info(problem)
+    log.info(parameters)
+    return SampleForTest(problem, parameters)
 
 
-def test_sampling():
-    problem, parameters = easy_sample()
-    # log.info(problem)
-    # log.info(parameters)
-
-def test_all_samples():
-    sample_functions = [easy_sample, sample_non_finite_gradient]
-    for s in sample_functions:
-        _test_fit_and_compute_consensus(s)
-
-def _test_fit_and_compute_consensus(sample_f):
-    problem, parameters = sample_f()
-    multinomial = StanMultinomialOptimizeConsensus()
-    consensus, parameters_learned = multinomial.fit_and_compute_consensus(problem)
-    tau_distance = distance(parameters_learned.tau, parameters.tau)
-    log.debug("Distance between learned tau and real tau: %f", tau_distance)
-    pi_distance = distance(parameters_learned.pi, parameters.pi)
-    log.debug("Distance between learned pi and real pi: %f", pi_distance)
-    assert close(parameters_learned.tau, parameters.tau)
-    assert close(parameters_learned.pi, parameters.pi)
+@pytest.fixture
+def sample_funcs() -> List[Callable]:
+    return [easy_sample, sample_non_finite_gradient]
 
 
-# TODO: Fill in the test methods below
+@pytest.fixture
+def samples(sample_funcs) -> List[SampleForTest]:
+    return [sf() for sf in sample_funcs]
 
-def test_fit():
-    pass
+
+def _test_fit_and_compute_consensus(model_cls: Type[AbstractStanOptimizeConsensus], sample: SampleForTest):
+    model = model_cls()
+    consensus, parameters_learned = model.fit_and_compute_consensus(sample.problem)
+    dict_parameters_learned = dataclasses.asdict(parameters_learned)
+    dict_sample_parameters = dataclasses.asdict(sample.parameters)
+    for p in dict_sample_parameters.keys():
+        log.debug("Distance between learned {p} and real {p}: {d:f}".format(
+            p=p, d=distance(dict_parameters_learned[p], dict_sample_parameters[p])))
+        assert close(dict_parameters_learned[p], dict_sample_parameters[p])
 
 
-def test_compute_consensus():
-    pass
+def _test_fit(model_cls: Type[AbstractStanOptimizeConsensus], sample: SampleForTest):
+    model = model_cls()
+    consensus_ref, parameters_ref = model.fit_and_compute_consensus(sample.problem)
+    model = model_cls()
+    parameters_learned = model.fit(sample.problem, consensus_ref)
+    dict_parameters_learned = dataclasses.asdict(parameters_learned)
+    dict_parameters_ref = dataclasses.asdict(parameters_ref)
+    for p in dict_parameters_learned.keys():
+        log.debug("Distance between learned {p} and reference {p}: {d:f}".format(
+            p=p, d=distance(dict_parameters_learned[p], dict_parameters_ref[p])))
+        assert close(dict_parameters_learned[p], dict_parameters_ref[p])
+
+
+def _test_compute_consensus(model_cls: Type[AbstractStanOptimizeConsensus], sample: SampleForTest):
+    model = model_cls()
+    consensus_ref, parameters_ref = model.fit_and_compute_consensus(sample.problem)
+    model = model_cls()
+    consensus_calculated, _ = model.compute_consensus(sample.problem, data=parameters_ref.to_json())
+    assert np.allclose(consensus_calculated, consensus_ref, atol=TOLERANCE)
+
+
+def test_multinomial_optimize_fit_and_compute_consensus(samples):
+    for sample in samples:
+        _test_fit_and_compute_consensus(StanMultinomialOptimizeConsensus, sample=sample)
+
+
+def test_multinomial_optimize_fit(samples):
+    for sample in samples:
+        _test_fit(StanMultinomialOptimizeConsensus, sample=sample)
+
+
+def test_multinomial_optimize_compute_consensus(samples):
+    for sample in samples:
+        _test_compute_consensus(StanMultinomialOptimizeConsensus, sample=sample)
