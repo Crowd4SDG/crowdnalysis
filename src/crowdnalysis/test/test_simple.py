1495459efdb5fa8f15a14763fe684c8687e281cb
Oguz Mulayim <oguz@iiia.csic.es>
2021-05-12 13:45:14 +0200

    apply new test framework to 'Probabilistic' &  'MajorityVoting' (#8)

--- src/crowdnalysis/test/test_simple.py
+++ src/crowdnalysis/test/test_simple.py
@@ -1,29 +1,24 @@
-from ..dawid_skene import DawidSkene
-from ..simple import Probabilistic, MajorityVoting
-from ..measures import Accuracy
 import numpy as np
 
+from .common import SampleForTest, BaseTestSimpleConsensusModel
+from ..dawid_skene import DawidSkene
+from ..measures import Accuracy
+from ..simple import Probabilistic, MajorityVoting
 
-def sample():
+
+def sample() -> SampleForTest:
     dgp = DawidSkene.DataGenerationParameters(n_tasks=1000, n_annotations_per_task=200)
     parameters = DawidSkene.Parameters(tau=np.array([0.3, 0.7]), pi=np.array([[[0.6, 0.3, 0.1], [0.3, 0.6, 0.1]]]))
     ds = DawidSkene()
     problem = ds.sample(dgp, parameters)
-
-    return problem, parameters
-
-
-def test_fit_and_compute_consensus():
-    pr = Probabilistic()
-    problem, params = sample()
-    consensus, _ = pr.fit_and_compute_consensus(problem)
-    assert Accuracy.evaluate(problem.f_T, consensus) == 1.0
-    # print(Accuracy.evaluate(problem.f_T, consensus))
-    # raise Exception()
+    return SampleForTest(problem, None)
 
 
-def test_majority_voting():
-    mv = MajorityVoting()
-    problem, params = sample()
-    consensus, _ = mv.fit_and_compute_consensus(problem)
-    assert Accuracy.evaluate(problem.f_T, consensus) == 1.0
+class TestProbabilisticConsensus(BaseTestSimpleConsensusModel):
+    model_cls = Probabilistic
+    sampling_funcs = [sample]
+
+
+class TestMajorityVotingConsensus(BaseTestSimpleConsensusModel):
+    model_cls = MajorityVoting
+    sampling_funcs = [sample]
