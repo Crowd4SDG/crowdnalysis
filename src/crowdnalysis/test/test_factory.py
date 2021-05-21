d48b2d806841a22f91d7a01991060c283c9c7949
Oguz Mulayim <oguz@iiia.csic.es>
2021-05-21 19:14:42 +0200

    incorporate 'factory' tests

---
+++ src/crowdnalysis/test/test_factory.py
@@ -0,0 +1,31 @@
+import pytest
+
+from ..consensus import AbstractSimpleConsensus
+from ..factory import Factory
+
+
+class DummyConsensus(AbstractSimpleConsensus):
+    name = "Dummy"
+
+
+class NotAnAbstractSimpleConsensus:
+    name = "Dummier"
+
+
+def test_all_factory_methods():
+    # Register
+    Factory.register_consensus_algorithm(DummyConsensus)
+    with pytest.raises(ValueError):
+        Factory.register_consensus_algorithm(NotAnAbstractSimpleConsensus)
+    # Get
+    assert Factory.get_consensus_algorithm(DummyConsensus.name) == DummyConsensus
+    with pytest.raises(KeyError):
+        Factory.get_consensus_algorithm(NotAnAbstractSimpleConsensus.name)
+    # Make
+    assert isinstance(Factory.make(DummyConsensus.name), DummyConsensus)
+    with pytest.raises(KeyError):
+        Factory.make(NotAnAbstractSimpleConsensus.name)
+    # Unregister
+    Factory.unregister_consensus_algorithm(DummyConsensus.name)
+    with pytest.raises(KeyError):
+        assert Factory.get_consensus_algorithm(DummyConsensus.name)
