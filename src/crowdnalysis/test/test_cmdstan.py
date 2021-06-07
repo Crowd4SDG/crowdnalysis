fffc4233df9d2b3f3f46ab144b8feb2208bc94ad
Oguz Mulayim <oguz@iiia.csic.es>
2021-06-07 11:20:39 +0300

    test Multinomial and MultinomialEta models separately

--- src/crowdnalysis/test/test_cmdstan.py
+++ src/crowdnalysis/test/test_cmdstan.py
@@ -25,17 +25,20 @@ def sample_non_finite_gradient() -> SampleForTest:
     log.info(parameters)
     return SampleForTest(problem, parameters)
 
+
 def more_labels_than_classes_sample() -> SampleForTest:
     dgp = StanMultinomialOptimizeConsensus.DataGenerationParameters(n_tasks=1000, n_annotations_per_task=20)
     parameters = StanMultinomialOptimizeConsensus.Parameters(tau=np.array([0.3, 0.7]),
-                                                             pi=np.array([[0.6, 0.1, 0.3], [0.3, 0.5, 0.2]]))
+                                                                pi=np.array([[0.6, 0.1, 0.3], [0.3, 0.5, 0.2]]))
     smoc = StanMultinomialOptimizeConsensus()
     problem = smoc.sample(dgp, parameters)
     return SampleForTest(problem, parameters)
 
-#class TestStanMultinomialOptimizeConsensus(BaseTestGenerativeConsensusModel):
-#    model_cls = StanMultinomialOptimizeConsensus
-#    sampling_funcs = [easy_sample, sample_non_finite_gradient, more_labels_than_classes_sample]
+
+class TestStanMultinomialOptimizeConsensus(BaseTestGenerativeConsensusModel):
+    model_cls = StanMultinomialOptimizeConsensus
+    sampling_funcs = [easy_sample, sample_non_finite_gradient]
+
 
 class TestStanMultinomialEtaOptimizeConsensus(BaseTestGenerativeConsensusModel):
     model_cls = StanMultinomialEtaOptimizeConsensus
