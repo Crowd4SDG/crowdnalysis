a0ca6b3c32528cef59343a32496379fd762dcc75
Oguz Mulayim <oguz@iiia.csic.es>
2021-07-02 17:09:22 +0300

    add visualization tests

--- src/crowdnalysis/test/test_data.py
+++ src/crowdnalysis/test/test_data.py
@@ -6,44 +6,9 @@ import pandas as pd
 import pytest
 from pandas.api.types import CategoricalDtype
 
+from .conftest import TEST
 from ..data import Data
 from ..problems import DiscreteConsensusProblem
-
-
-class TEST:
-    TASK_IDS = [12, 10, 11]
-    TASK_KEYS = [f"t{i}" for i in TASK_IDS]
-    TASK_RUN_IDS = [10, 11, 12, 11, 12, 11, 10, 10, 12, 10, 11, 12]
-    USER_IDS = ["u2", "u0", "u1", "u1", "u2", "u2", "u0", "u1", "u0", "u3", "u3", "u3"]
-    QUESTIONS = ["question_0", "question_1"]
-    CATEGORIES = {QUESTIONS[0]: CategoricalDtype(categories=["Yes", "No", "Not sure"], ordered=False),
-                  QUESTIONS[1]: CategoricalDtype(categories=["A", "B", "C", "Not answered"], ordered=False)}
-    ANSWER_0 = ["No", "No", "Yes", "Yes", "Not sure", "Yes", "Not sure", "Yes", "No", "No", "Yes", "No"]
-    ANSWER_1 = ["C", "Not answered", "B", "B", "C", "A", "B", "A", "B", "Not answered", "A", "A"]
-    EXTRA_COL = "extra_col"
-    EXTRA_COL_VAL = ["".join(random.sample("XYZ", 3)) for i in range(len(TASK_RUN_IDS))]
-
-
-@pytest.fixture(scope="session")
-def fixt_single_file_records() -> np.ndarray:
-    return np.array([(TEST.USER_IDS[i], TEST.TASK_RUN_IDS[i], TEST.ANSWER_0[i], TEST.ANSWER_1[i], TEST.EXTRA_COL_VAL[i])
-                     for i in range(len(TEST.TASK_RUN_IDS))],
-                    dtype=[(Data.COL_USER_ID, "U15"), (Data.COL_TASK_ID, "i4"),
-                           (TEST.QUESTIONS[0], "U15"), (TEST.QUESTIONS[1], "U15"), (TEST.EXTRA_COL, "U15")])
-
-
-@pytest.fixture(scope="session")
-def fixt_df(fixt_single_file_records):
-    df = pd.DataFrame.from_records(fixt_single_file_records)
-    return df
-
-
-@pytest.fixture(scope="session")
-def fixt_data(fixt_df) -> Data:
-    d = Data.from_df(fixt_df, data_src="test", questions=TEST.QUESTIONS, task_ids=TEST.TASK_IDS,
-                     categories=TEST.CATEGORIES, annotator_id_col_name=Data.COL_USER_ID)
-    # print("\n", d.df)
-    return d
 
 
 @pytest.fixture(scope="module")
---
+++ src/crowdnalysis/test/test_visualization.py
@@ -0,0 +1,100 @@
+from html.parser import HTMLParser
+from unittest.mock import mock_open, patch
+# TODO (OM, 20210702): Replace unittest with pytest, if a solid way of mocking 'file write operations' is found for the latter.)
+
+import pandas as pd
+import pytest
+
+from .conftest import TEST
+from ..factory import Factory
+from ..visualization import csv_description, html_description, html_description_crossed
+
+
+question = TEST.QUESTIONS[0]
+picture_field = TEST.EXTRA_COL
+
+
+class MyHTMLParser(HTMLParser):
+
+    def __init__(self):
+        self.start_tags = []
+        self.end_tags = []
+        super().__init__()
+
+    def handle_starttag(self, tag, attrs):
+        self.start_tags.append(tag)
+
+    def handle_endtag(self, tag):
+        self.end_tags.append(tag)
+
+
+@pytest.fixture(scope="module")
+def fixt_consensus(fixt_data):
+    model = Factory.make("MajorityVoting")
+    consensus, _ = model.fit_and_compute_consensus(fixt_data.get_dcp(question))
+    return consensus
+
+
+def test_html_description(fixt_consensus, fixt_data):
+
+    mocked_file_path = "mock/file/path/spam.html"
+    with patch("builtins.open", mock_open()) as mocked_file:
+        html = html_description(consensus=fixt_consensus, data=fixt_data, question=question,
+                                picture_field=picture_field,
+                                output_file=mocked_file_path)
+
+        # assert if opened file on write mode 'w'
+        mocked_file.assert_called_once_with(mocked_file_path, 'w')
+
+        # assert if the html content was written in file
+        mocked_file().write.assert_called_once_with(html)
+
+    # Assert generated HTML is valid
+    parser = MyHTMLParser()
+    parser.feed(html)
+
+    assert (parser.start_tags[0].lower() == "html")
+    assert (parser.end_tags[-1].lower() == "html")
+    assert(sorted(parser.start_tags) == sorted(parser.end_tags))  # all tags are closed, valid XHTML
+
+
+def test_html_description_crossed(fixt_consensus, fixt_data):
+    compare_task_indices = list(range(len(TEST.TASK_IDS)))[1:]
+    mocked_file_path = "mock/file/path/spam_crossed.html"
+    with patch("builtins.open", mock_open()) as mocked_file:
+        html = html_description_crossed(consensus=fixt_consensus, data=fixt_data, question=question,
+                                picture_field=picture_field, compare_task_indices=compare_task_indices,
+                                lbl_outline="spam", lbl_actor="eggs", output_file=mocked_file_path)
+
+        # assert if opened file on write mode 'w'
+        mocked_file.assert_called_once_with(mocked_file_path, 'w')
+
+        # assert if the html content was written in file
+        mocked_file().write.assert_called_once_with(html)
+
+    # Assert generated HTML is valid
+    parser = MyHTMLParser()
+    parser.feed(html)
+
+    assert (parser.start_tags[0].lower() == "html")
+    assert (parser.end_tags[-1].lower() == "html")
+    assert(sorted(parser.start_tags) == sorted(parser.end_tags))
+
+    html2 = html_description(consensus=fixt_consensus, data=fixt_data, question=question, picture_field=picture_field)
+    parser2 = MyHTMLParser()
+    parser2.feed(html2)
+
+    assert(len(parser.start_tags) > len(parser2.start_tags))
+
+
+def test_csv_description(fixt_consensus, fixt_data):
+    mocked_file_path = "mock/file/path/spam.csv"
+    with patch.object(pd.DataFrame, "to_csv") as mocked_file:
+        csv = csv_description(consensus=fixt_consensus, data=fixt_data, question=question,
+                              picture_field=picture_field, output_file=mocked_file_path)
+
+        # assert if the html content was written in file
+        mocked_file.assert_called_with(mocked_file_path)
+
+
+
