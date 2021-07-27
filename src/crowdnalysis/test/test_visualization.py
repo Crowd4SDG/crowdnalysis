c5e8d5520a92a180b35c178f83bb89889a81a685
Oguz Mulayim <oguz@iiia.csic.es>
2021-07-27 17:52:37 +0300

    cosmetic linting

--- src/crowdnalysis/test/test_visualization.py
+++ src/crowdnalysis/test/test_visualization.py
@@ -1,6 +1,6 @@
 from html.parser import HTMLParser
 from unittest.mock import mock_open, patch
-# TODO (OM, 20210702): Replace unittest with pytest, if a solid way of mocking `builtins.open` is found for the latter.)
+# TODO (OM, 20210702): Replace unittest with pytest, if a solid way of mocking `builtins.open` is found for the latter.
 
 import pandas as pd
 import pytest
@@ -28,7 +28,7 @@ class HTMLParserForTest(HTMLParser):
         self.end_tags.append(tag)
 
 
-def check_valid_html(html: str) -> HTMLParserForTest:
+def assert_valid_html(html: str) -> HTMLParserForTest:
     """Asserts if a given string is valid html"""
     parser = HTMLParserForTest()
     parser.feed(html)
@@ -48,19 +48,16 @@ def fixt_consensus(fixt_data):
 
 
 def test_html_description(fixt_consensus, fixt_data):
-
     mocked_file_path = "mock/file/path/spam.html"
     with patch("builtins.open", mock_open()) as mocked_file:
         html = html_description(consensus=fixt_consensus, data=fixt_data, question=question,
-                                picture_field=picture_field,
-                                output_file=mocked_file_path)
-
+                                picture_field=picture_field, output_file=mocked_file_path)
         # assert if opened file on write mode 'w'
         mocked_file.assert_called_once_with(mocked_file_path, 'w')
-
         # assert if the html content was written in file
         mocked_file().write.assert_called_once_with(html)
-    check_valid_html(html)
+    # assert generated HTML is valid
+    assert_valid_html(html)
 
 
 def test_html_description_crossed(fixt_consensus, fixt_data):
@@ -68,22 +65,19 @@ def test_html_description_crossed(fixt_consensus, fixt_data):
     mocked_file_path = "mock/file/path/spam_crossed.html"
     with patch("builtins.open", mock_open()) as mocked_file:
         html = html_description_crossed(consensus=fixt_consensus, data=fixt_data, question=question,
-                                picture_field=picture_field, compare_task_indices=compare_task_indices,
-                                lbl_outline="spam", lbl_actor="eggs", output_file=mocked_file_path)
-
+                                        picture_field=picture_field, compare_task_indices=compare_task_indices,
+                                        lbl_outline="spam", lbl_actor="eggs", output_file=mocked_file_path)
         # assert if opened file on write mode 'w'
         mocked_file.assert_called_once_with(mocked_file_path, 'w')
-
         # assert if the html content was written in file
         mocked_file().write.assert_called_once_with(html)
-
-    # Assert generated HTML is valid
-    parser = check_valid_html(html)
+    # assert generated HTML is valid
+    parser = assert_valid_html(html)
 
     html2 = html_description(consensus=fixt_consensus, data=fixt_data, question=question, picture_field=picture_field)
     parser2 = HTMLParserForTest()
     parser2.feed(html2)
-
+    # assert `html` has more tags than `html2` due to `compare_task_indices`
     assert(len(parser.start_tags) > len(parser2.start_tags))
 
 
@@ -92,9 +86,5 @@ def test_csv_description(fixt_consensus, fixt_data):
     with patch.object(pd.DataFrame, "to_csv") as mocked_file:
         csv = csv_description(consensus=fixt_consensus, data=fixt_data, question=question,
                               picture_field=picture_field, output_file=mocked_file_path)
-
         # assert if the html content was written in file
         mocked_file.assert_called_with(mocked_file_path)
-
-
-
