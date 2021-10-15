c5b1edd5680d9761d3218aefc659afa86c25ad15
Oguz Mulayim <oguz@iiia.csic.es>
2021-10-15 12:40:10 +0300

    set cmdstan and cmdstanpy versions explicitly

--- bin/init-local.sh
+++ bin/init-local.sh
@@ -10,12 +10,13 @@ pip install -r requirements.txt
 export PYTHONPATH=$PYTHONPATH:${PWD}/src
 
 CMDSTAN_DIR="$HOME/.cmdstan"
+CMDSTAN_VER="2.26.1"
 if [[ "$OSTYPE" == "linux-gnu"*  ||  "$OSTYPE" == "darwin"* ]]; then
   if [[ ! -d "$CMDSTAN_DIR" ]]; then
     echo "Installing CmdStan library into '${CMDSTAN_DIR}', please wait..."
-    install_cmdstan -d $CMDSTAN_DIR
-    echo "CmdStan library installed."
+    install_cmdstan -d $CMDSTAN_DIR  -v $CMDSTAN_VER
+    echo "CmdStan library installed (v${CMDSTAN_VER})."
   else
-    echo "CmdStan library already installed in '${CMDSTAN_DIR}'."
+    echo "CmdStan library (v${CMDSTAN_VER}) already installed in '${CMDSTAN_DIR}'."
   fi
 fi
