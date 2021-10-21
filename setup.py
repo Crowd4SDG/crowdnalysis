69169afe66f7d8c35ca3d3a314f5c7491bc1eec1
Oguz Mulayim <oguz@iiia.csic.es>
2021-10-21 16:06:10 +0200

    Add keywords, Python versions, and Tutorial URL

--- setup.py
+++ setup.py
@@ -11,12 +11,24 @@ HERE = os.path.dirname(__file__)
 PACKAGE_DIR = "src"
 
 CLASSIFIERS = """
-Programming Language :: Python :: 3
 License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)
 Operating System :: OS Independent
 Intended Audience :: Science/Research
 Topic :: Scientific/Engineering :: Information Analysis
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
 """
+
+HOMEPAGE = "https://github.com/Crowd4SDG/crowdnalysis"
+
+PROJECT_URLS = {
+    "Tutorial": "https://github.com/Crowd4SDG/crowdnalysis/blob/master/nb/tutorial.ipynb",
+    "Bug Tracker": "https://github.com/Crowd4SDG/crowdnalysis/issues"
+}
+
+KEYWORDS = "citizen-science crowdsourcing annotation-aggregation annotator-model prospective-analysis"
 
 
 def readme() -> str:
@@ -44,10 +56,11 @@ setup(
     description="Library to help analyze crowdsourcing results",
     long_description=readme(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Crowd4SDG/crowdnalysis",
-    project_urls={"Bug Tracker": "https://github.com/Crowd4SDG/crowdnalysis/issues"},
+    url=HOMEPAGE,
+    project_urls=PROJECT_URLS,
     install_requires=requirements(),
     classifiers=CLASSIFIERS.strip().split('\n'),
+    keywords=KEYWORDS,
     package_dir={"": PACKAGE_DIR},
     packages=find_packages(where=PACKAGE_DIR),
     package_data={PACKAGE_NAME: ['cmdstan/cmdstan/*.stan']},
