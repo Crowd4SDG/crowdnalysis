2ffd25c492dc3d6ac70edc083df5fba6ee0535ae
Oguz Mulayim <oguz@iiia.csic.es>
2022-02-28 18:57:18 +0100

    support 'CmdStan' latest (#16)

--- bin/init-local.sh
+++ bin/init-local.sh
@@ -10,13 +10,12 @@ pip install -r requirements.txt
 export PYTHONPATH=$PYTHONPATH:${PWD}/src
 
 CMDSTAN_DIR="$HOME/.cmdstan"
-CMDSTAN_VER="2.26.1"
 if [[ "$OSTYPE" == "linux-gnu"*  ||  "$OSTYPE" == "darwin"* ]]; then
   if [[ ! -d "$CMDSTAN_DIR" ]]; then
     echo "Installing CmdStan library into '${CMDSTAN_DIR}', please wait..."
-    install_cmdstan -d $CMDSTAN_DIR  -v $CMDSTAN_VER
-    echo "CmdStan library installed (v${CMDSTAN_VER})."
+    install_cmdstan -d $CMDSTAN_DIR
+    echo "CmdStan library installed."
   else
-    echo "CmdStan library (v${CMDSTAN_VER}) already installed in '${CMDSTAN_DIR}'."
+    echo "CmdStan library already installed at '${CMDSTAN_DIR}'."
   fi
 fi
